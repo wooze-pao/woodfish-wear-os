a97a37d8829abf78e3bcb408a82a08872ccb8c1f
Woozepao <xiaobbs@outlook.com>
2025-06-05 12:53:11 -0500

    1.3.1版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.wooze.wear.woodfish"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
     }
 
